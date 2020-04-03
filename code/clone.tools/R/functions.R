daa75400675439f50aca870b37d221a8d874b33e
llyzhng <lzheng163@gmail.com>
2020-04-03 15:49:57 -0400

    Implement spike and slab prior for MCF, test on simulated data with clusters with low MCFs

--- code/clone.tools/R/functions.R
+++ code/clone.tools/R/functions.R
@@ -48,6 +48,56 @@ simulateData <- function(I, K, S, avg.cov=100){
     test.data
 }
 
+simulateData3 <- function(I, K, S, avg.cov=100){
+  pi <- rep(1/K, K)
+  ##z <- sample(1:K, size = I, replace = T, prob = pi)
+  ## True cancer cell fraction
+  z <- rep(1:K, each=I/K)
+  w <- matrix(c(0.98, 0.99, 0.97, 
+                0.98, 0.90, 0.82,
+                0.55, 0.00, 0.80, 
+                0.20, 0.00, 0.50,
+                0.30, 0.00, 0.30, 
+                0.43, 0.90, 0.00,
+                0.30, 0.70, 0.00,
+                0.06, 0.00, 0.00,
+                0.00, 0.00, 0.08,
+                0.00, 0.07, 0.00),
+              byrow=T,
+              nrow=K, ncol=S)
+  
+  colnames(w) <-  paste0("sample", 1:S)
+  
+  tcn <- matrix(2, nrow=I, ncol=S)
+  m <- matrix(rep(sample(1:2, size = I, replace = T), S), 
+              nrow=I, ncol=S)
+  W <- w[z, ]
+  
+  theta <- calcTheta(m, tcn, W)
+  ##
+  ## Simulate altered reads
+  ##
+  n <- replicate(S, rpois(I, avg.cov))
+  y <- matrix(NA, nrow=I, ncol=S)
+  for (i in 1:I) {
+    for (s in 1:S) {
+      y[i, s] <- rbinom(1, n[i, s], theta[i,s])
+    }
+  }
+  
+  p <- y/n
+  colnames(w) <- colnames(p) <- paste0("sample", 1:S)
+  colnames(m) <- colnames(p)
+  test.data <- list("I" = I, "S" = S, "K" = K, 
+                    "y" = y, "n" = n,
+                    "m" = m, "tcn" = tcn,
+                    z=z,
+                    w=w,
+                    theta=theta,
+                    p=p)
+  test.data
+}
+
 simulateData2 <- function(I, K, S){
   pi <- rep(0.1, 10)
   ##z <- sample(1:K, size = I, replace = T, prob = pi)
@@ -136,7 +186,7 @@ calcLogLik <- function(z.iter, w.iter, input.data) {
 }
 
 calcChainLogLik <- function(samps, input.data, K) {
-  z.chain <- getParamChain(samps, "z")
+  z.chain <- getParamChain(samps, "z\\[")
   w.chain <- getParamChain(samps, "w")
   lik <- c()
   for(iter in 1:nrow(z.chain)) {
