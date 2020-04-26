f171f35a73aacdaefd36ad504919f31478e31084
Lily Zheng <lzheng163@gmail.com>
2020-04-25 20:03:49 -0400

    update readme

--- code/server/readme.sh
+++ code/server/readme.sh
@@ -1,6 +1,13 @@
 # make conda environment using environment.yml
 conda env create -f environment.yml
 
-# tree MH (with weighted mutate, 10^tier) on simulated data -- 5 different chains
+# tree MH (with weighted mutate, 10^tier) on simulated data
 mh_trees_sim1_mutate10.R 
+# tree MH (plain mutate)
+mh_trees_sim1.R
+
+# run tree mh --  5 different chains
 ./run-trees-sim1.sh
+
+# plot tree mh results -- change mh_trees script
+./run-tree-plots.sh
