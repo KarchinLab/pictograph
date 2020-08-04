e3b9c7c70d96d66a331d4ff6dfb5cff41ba13281
llyzhng <lzheng163@gmail.com>
2020-08-04 10:22:02 -0400

    move metrics related functions to separate file

---
+++ code/clone.tools/R/metrics.R
@@ -0,0 +1,100 @@
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# Tree metric: proportion of correct ancestral relationships 
+
+getParent <- function(node, am.long) {
+  am.long <- am.long %>%
+    mutate(parent = as.character(parent))
+  e <- am.long %>% 
+    filter(child == node, connected == 1)
+  e$parent
+}
+
+getPathFromRoot <- function(node, am.long) {
+  path <- node
+  curr_parent <- getParent(node, am.long)
+  while(curr_parent != "root") {
+    path <- c(path, curr_parent)
+    curr_parent <- getParent(curr_parent, am.long)
+  }
+  as.numeric(path)
+}
+
+getRelationshipTypeOfClusters <- function(cluster1, cluster2, am.long) {
+  # input: cluster1 and cluster2 can be either characters or numeric 
+  # returns type of ancestral relationship of cluster1 and cluster2
+  #     1 = cluster1 and cluster2 are the same
+  #     2 = cluster1 is ancestral to cluster2
+  #     3 = cluster2 is ancestral to cluster1
+  #     4 = cluster1 and cluster2 are on distinct branches
+  
+  if (is.numeric(cluster1)) cluster1 <- as.character(cluster1)
+  if (is.numeric(cluster2)) cluster2 <- as.character(cluster2)
+  
+  # clusters are the same
+  if (cluster1 == cluster2) return(1)
+  
+  # cluster1 is ancestral to cluster2
+  temp_path <- getPathFromRoot(cluster2, am.long)
+  if (cluster1 %in% temp_path) return(2)
+  
+  # cluster2 is ancestral to cluster1
+  temp_path <- getPathFromRoot(cluster1, am.long)
+  if (cluster2 %in% temp_path) return(3)
+  
+  # cluster1 and cluster2 are on distinct branches
+  return(4)
+}
+
+getMutRelTb <- function(z, mut_ind, am) {
+  # input: 
+  # z = vector of cluster assignments for each mutation
+  # mut_ind = vector of mutation indices e.g. 1:I
+  # am = am.long format of graph 
+  # output: tibble with columns mut1, mut2, cluster1, cluster2, relationship_type
+  # relationship_type is 1:4 -- see getRelationshipTypeOfCluster()
+  all_mut_pairs <- rbind(t(combn(mut_ind, 2)), t(combn(rev(mut_ind), 2)))
+  all_cluster_pairs <- rbind(t(combn(unique(z), 2)), t(combn(rev(unique(z)), 2)),
+                             matrix(rep(unique(z), 2), nrow=length(unique(z)), byrow = F))
+  
+  all_cluster_pairs_tb <- tibble(cluster1 = all_cluster_pairs[, 1],
+                                 cluster2 = all_cluster_pairs[, 2])
+  
+  true_rel_types <- mapply(function(c1, c2) getRelationshipTypeOfClusters(c1, c2, am), 
+                           c1=all_cluster_pairs[, 1], c2=all_cluster_pairs[, 2])
+  
+  rel_tb <- tibble(cluster1 = all_cluster_pairs[, 1],
+                   cluster2 = all_cluster_pairs[, 2],
+                   relationship_type = true_rel_types)
+  
+  true_mut_rel <- tibble(mut1 = all_mut_pairs[, 1],
+                         mut2 = all_mut_pairs[, 2],
+                         cluster1 = z[all_mut_pairs[, 1]],
+                         cluster2 = z[all_mut_pairs[, 2]])
+  
+  full_join(true_mut_rel, rel_tb, by = c("cluster1", "cluster2"))
+}
+
+calcPropRelationshipsCorrect <- function(test_mut_rel, true_mut_rel) {
+  # input: tibbles from getMutRelTb()
+  # returns proportion of relationships in test_mut_rel that match true_mut_rel
+  test_comp <- full_join(true_mut_rel, test_mut_rel, by = c("mut1", "mut2"))
+  rel_same <- test_comp$relationship_type.x == test_comp$relationship_type.y
+  return(sum(rel_same) / length(rel_same))
+}
+
+# -----------------------------------------------------------------------------
+# -----------------------------------------------------------------------------
+# CCF metrics (as described in PASTRI paper)
+
+# Metric 1 -- A measure of sensitivity, matches the true clones to the nearest 
+#   reported clusters
+calcMetric1 <- function() {
+  
+}
+
+# Metric 2 -- A measure of specificity, matches the reported clusters to the 
+#   nearest true clones 
+calcMetric2 <- function() {
+  
+}
