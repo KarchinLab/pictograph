434b063ab6199d74a29002b69a5fdc76248d011a
llyzhng <lzheng163@gmail.com>
2020-12-18 10:16:50 -0500

    fix variable name error

--- code/clone.tools/R/gabowmyers.R
+++ code/clone.tools/R/gabowmyers.R
@@ -197,7 +197,7 @@ checkEdge <- function(edge, w, thresh = 0.2) {
 calcTreeSpaceUpperBound <- function(w, zero.thresh = 0.01, 
                                     lineage.precedence.filter = 0.1) {
   graph_G_pre <- prepareGraphForGabowMyers(w, zero.thresh = zero.thresh)
-  graph_G <- filterEdgesBasedOnCCFs(graph_G_pre, sim_data$w, thresh = lineage.precedence.filter)
+  graph_G <- filterEdgesBasedOnCCFs(graph_G_pre, w, thresh = lineage.precedence.filter)
   num_edges_to_each_child <- graph_G %>% 
     group_by(child) %>%
     summarize(n = n()) %>%
